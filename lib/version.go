70a1436202c1c9cf64620096e0bb81ae2c4437f0
Natrim <natrim@users.noreply.github.com>
2026-04-21 13:52:27 +0200

    try to get better version number

--- lib/version.go
+++ lib/version.go
@@ -2,12 +2,17 @@ package lib
 
 import (
 	"os/exec"
+	"runtime/debug"
 	"strings"
 	"time"
 )
 
 // Version will contain nrb build number on build
 var Version = "dev"
+
+func init() {
+	Version = fixBuild(Version)
+}
 
 // ParseVersion will use git to get current revision to use as version string
 func ParseVersion() string {
@@ -17,4 +22,37 @@ func ParseVersion() string {
 	}
 
 	return time.Now().Format("v20060102150405")
+}
+
+// try to get itnernal build name
+func fixBuild(v string) string {
+	if strings.HasPrefix(v, "v") {
+		return v
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return v
+	}
+
+	if strings.HasPrefix(info.Main.Version, "v") {
+		return info.Main.Version
+	}
+
+	for _, setting := range info.Settings {
+		if setting.Key == "vcs.revision" {
+			v = setting.Value
+			return v
+		}
+	}
+
+	// If we didn't find vcs.revision, try the old key for backward compatibility
+	for _, setting := range info.Settings {
+		if setting.Key == "gitrevision" {
+			v = setting.Value
+			return v
+		}
+	}
+
+	return v
 }
